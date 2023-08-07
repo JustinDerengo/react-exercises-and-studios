0fabd5accd91c44b9b7398ba503d1527a54f594b
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-08-07 08:57:06 -0600

    Exercise 1 part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Recommend";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1659905815i/228665.jpg";
+   let book2 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg";
+   let book3 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1617768316i/68428.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The wheel of time" width="200px"/>
+         <img src={book2} alt="Dune" width ="200px"/>
+         <img src={book3} alt="Mistborn" width="200px"/>
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <div>
+      <h3 className = {classes.choresHeading}>Chores to do</h3>
+      <ol>
+         <li className = {classes.choresText}>Vaccum the trash</li>
+         <li className = {classes.choresText}>Feed the living room</li>
+         <li className = {classes.choresText}>Take out the cat</li>
+      </ol>
+   </div>
+   );
 }
